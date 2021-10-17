c8e2a343a18e52164f28e653f4f9029f374da2a3
Alexgv1108 <alexgallego1108@gmail.com>
2021-10-16 22:20:49 -0500

    Se modifican rutas de la página

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Home from './components/Home';
-import Hojavida from './components/Hojavida';
 
 function App() {
   return (
     <BrowserRouter> 
       <Switch>
-        <Route component={Home} path="/" asename={process.env.PUBLIC_URL}/>
-        <Route exact path="/documents/hojavida" component={Hojavida} />
+        <Route exact component={Home} path="/" asename={process.env.PUBLIC_URL}/>
       </Switch>
     </BrowserRouter>
   )
